a5cd61030130e1d55a6ad20e19400ca6710991a5
KDragon <elkarouani@gmail.com>
2019-01-15 00:19:56 +0100

    Task: caching Dom for future use

---
+++ Simon-game/js/index.js
@@ -0,0 +1,20 @@
+let order = [];
+let playerOrder = [];
+let flash;
+let turn;
+let good;
+let compTurn;
+let intervalId;
+let strict = false;
+let noise = true;
+let on = false;
+let win;
+
+const turnCounter = document.querySelector("#turn");
+const topLeft = document.querySelector("#topleft");
+const topRight = document.querySelector("#topright");
+const bottomLeft = document.querySelector("#bottomleft");
+const bottomRight = document.querySelector("#bottomright");
+const strictButton = document.querySelector("#strict");
+const onButton = document.querySelector("#on");
+const startButton = document.querySelector("#start");
